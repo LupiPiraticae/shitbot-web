0b11179dafc71f11797dadcb66de99d057e43821
Alexander Wolf <alexander.wolf@8mylez.com>
2021-10-08 22:55:02 +0200

    Add basic controller for website data

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,5 +15,33 @@ class IndexController extends AbstractController
     public function index(): Response
     {
         return $this->render('base.html.twig', []);
+    }
+
+    /**
+     * @Route("/facility/list", name="facility.list")
+     *
+     * @return Response
+     */
+    public function getFacilities(): Response
+    {
+        $facilities = [
+            [
+                'id' => 1,
+                'name' => 'Klo 1',
+                'position' => 'left'
+            ],
+            [
+                'id' => 2,
+                'name' => 'Klo 2',
+                'position' => 'middle'
+            ],
+            [
+                'id' => 3,
+                'name' => 'Klo 3',
+                'position' => 'right'
+            ]
+        ];
+
+        return new JsonResponse($facilities);
     }
 }
