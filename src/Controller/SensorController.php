ced881460fa91556159517962b54c62c59465b7a
Alexander Wolf <alexander.wolf@8mylez.com>
2021-10-09 01:31:34 +0200

    Basic sensor controller

--- src/Controller/SensorController.php
+++ src/Controller/SensorController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\FacilityRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -9,11 +11,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class SensorController extends AbstractController
 {
     /**
-     * @Route("/facility/update", name="facility.update")
+     * @Route("/facility/{id}/update", name="facility.update", methods={"POST"})
      */
-    public function updateFacility(): Response
+    public function updateFacility(string $id, Request $request, FacilityRepository $facilityRepository): Response
     {
-        echo 'UPDATED FACILITY';
+
+        //get body
+        $data = json_decode($request->getContent(), true);
+        print_r($data);
+        die();
+//
+//
+//        $facility = $facilityRepository->find($id);
+//
+//        if ($facility) {
+//            $facility->setState()
+//        }
+//        echo "Id is: " . $id;
         return new Response();
     }
 }
